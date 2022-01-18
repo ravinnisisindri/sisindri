64373f7e058143b1641c6e278c1492ed4bfdc1fe
ravinnisisindri <96733202+ravinnisisindri@users.noreply.github.com>
2022-01-18 16:30:06 +0530

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,14 @@
+
+
+public class Main {
+	
+	/*Enter your desired Directory path */
+	public static final String path = "C:\\phase1"; 
+	
+	public static void main(String[] args) {
+		Menus menu = new Menus();
+		menu.introScreen();
+		menu.mainMenu();
+	}
+	
+}
