2329ccda55d574372409f0111bf06b3ff9f8eca4
ravinnisisindri <96733202+ravinnisisindri@users.noreply.github.com>
2022-02-21 13:35:42 +0530

    Add files via upload

---
+++ HTTPS for Spring Boot.java
@@ -0,0 +1,39 @@
+Creating MainController for showing a page in the browser under SSL: Creating MainController for showing a page in the browser under SSL
+package com.ecommerce.controllers;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+
+
+
+@Controller
+public class MainController {
+
+         @Autowired
+         private ProductRepository repository;
+         
+         @RequestMapping("/")
+          @ResponseBody
+          public String index() {
+               
+
+            return “This is running under SSL”;
+          }
+         
+        
+}
+Configuring application.properties to run the site in SSL
+server.port=8443
+server.ssl.key-alias=selfsigned_localhost_sslserver
+server.ssl.key-password=changeit
+server.ssl.key-store=classpath:ssl-server.jks
+server.ssl.key-store-provider=SUN
+server.ssl.key-store-type=JKS
